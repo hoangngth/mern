f2d300daa4dbd976672a329e211953ef6428e697
Nguyen The Hoang <hoang.nth1996@gmail.com>
2022-03-25 15:15:35 +0700

    finish section 12

--- frontend/src/shared/context/auth-context.js
+++ frontend/src/shared/context/auth-context.js
@@ -1,9 +1,10 @@
-import { createContext } from 'react';
+import { createContext } from "react";
 
 export const AuthContext = createContext({
   isLoggedIn: false,
   userId: null,
+  name: null,
   token: null,
   login: () => {},
-  logout: () => {}
+  logout: () => {},
 });
